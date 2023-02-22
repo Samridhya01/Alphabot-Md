e5fccd57b9ed32ad307263de02659429f9bfbe02
Samridhya01 <125868472+Samridhya01@users.noreply.github.com>
2023-02-22 15:03:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const chalk = require('chalk')
-const  { indonesia, english, spanyol} = require(`./language`)
+const  { english} = require(`./language`)
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
